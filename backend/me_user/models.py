2a6e3771d8401ca657ff06e5b4c4edea4039e434
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2019-11-09 18:30:06 -0500

    pushing

--- backend/me_user/models.py
+++ backend/me_user/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
@@ -12,3 +13,14 @@ class MeUser(AbstractBaseUser, PermissionsMixin):
 	objects = MeUserManager()
 
 	USERNAME_FIELD = 'username'
+
+class MeLink(models.Model):
+	title = models.CharField(_('title'), max_length=255, blank=True)
+	link = models.URLField(_('link'), max_length=200, blank=True)
+
+class MePost(models.Model):
+	title = models.CharField(_('title'), max_length=255, blank=True)
+	author = models.ForeignKey(MeUser, on_delete=models.CASCADE)
+
+	date_created = models.DateTimeField(_('date created'), default=timezone.now)
+	links = models.ManyToManyField(MeLink, related_name='post_links')
