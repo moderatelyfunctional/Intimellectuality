3d059bbc1cb6bfb48791ed8884d8139e62210827
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2019-11-09 20:54:44 -0500

    me

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,6 +25,8 @@ SECRET_KEY = '6k^7=u_bi0y-0$9lsrg7df9(j346uz1)=_i-q+1((h-k30#9q^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CORS_ORIGIN_ALLOW_ALL = DEBUG
+
 ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'me_user.MeUser'
